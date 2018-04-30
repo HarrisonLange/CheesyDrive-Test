09c8a18e7f434423113225a4d9970cbda13e041b
Harrison <HarrisonRLange@gmail.com>
2018-04-29 21:20:31 -0700

    Fixed

--- BasicDrive/src/org/usfirst/frc/team4738/robot/Robot.java
+++ BasicDrive/src/org/usfirst/frc/team4738/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc.team4738.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -12,13 +5,6 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import org.usfirst.frc.team4738.robot.subsystems.DriveTrain_SS;
 import org.usfirst.frc.team4738.robot.subsystems.Lift_SS;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
 public class Robot extends TimedRobot {
 	public static OI m_oi;
 	public static DriveTrain_SS DriveTrain = new DriveTrain_SS();
@@ -43,9 +29,6 @@ public class Robot extends TimedRobot {
 	public void autonomousInit() {
 	}
 
-	/**
-	 * This function is called periodically during autonomous.
-	 */
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
@@ -60,9 +43,6 @@ public class Robot extends TimedRobot {
 		Scheduler.getInstance().run();
 	}
 
-	/**
-	 * This function is called periodically during test mode.
-	 */
 	@Override
 	public void testPeriodic() {
 	}
